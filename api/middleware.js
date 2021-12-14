c0a57ff40c2ac528269e5c6428383c9eaa9c48b4
vencax <vencax@centrum.cz>
2021-12-14 13:23:34 +0100

    fix

--- api/middleware.js
+++ api/middleware.js
@@ -28,14 +28,14 @@ export default function (ctx) {
         const r = await getQB(knex, TNAMES.MSHIPS, schema).insert({uid, gid})
         return r
       } catch (err) {
-        throw new APIError(400, err.toString())
+        throw new ErrorClass(400, err.toString())
       }
     },
     removeFromGroup: function (gid, uid, schema) {
       try {
         return getQB(knex, TNAMES.MSHIPS, schema).where({uid, gid}).del()
       } catch (err) {
-        throw new APIError(400, err.toString())
+        throw new ErrorClass(400, err.toString())
       }
     },
     listGroup: function (gid, schema) {
